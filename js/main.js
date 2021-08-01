f9bfd9979f606321efe4120dc6f7ac8a1ecdd6c6
Igor Kislitsyn <k.yngvarr@gmail.com>
2021-08-02 00:56:44 +0700

    with no sound

--- js/main.js
+++ js/main.js
@@ -1,2 +1,138 @@
+const DEFAULT_TIME = 1 * 60 * 1000; // minutes
+const COOLDOWN_TIME = 50 * 1000;
+
+let time = 0;
+let t0;
+
+let timer_element;
+let ctrl_button;
+let interval_id;
+
+function pad_digit(t) {
+    return t < 10 ? `0${t}` : `${t}`
+}
+
+function time_string(t) {
+    return `${(t / 60000)|0}:${pad_digit((t / 1000 % 60)|0)}`;
+}
+
+function update_text(t) {
+    timer_element.innerText = time_string(t);
+}
+
+// -------- STATE MACHINE --------
+
+class State {
+    constructor({enter, exit, button_label, text_class}) {
+        this.onEnter = enter ?? null;
+        this.onExit = exit ?? null;
+        this.text_class = text_class ?? null;
+        this.button_label = button_label ?? null;
+    }
+    enter() {
+        if (this.text_class) {
+            timer_element.classList.add(this.text_class);
+        }
+        if (this.button_label) {
+            ctrl_button.innerText = this.button_label;
+        }
+        if (this.onEnter) this.onEnter();
+    }
+    exit() {
+        if (this.text_class) {
+            timer_element.classList.remove(this.text_class);
+        }
+        if (this.onExit) this.onExit();
+    }
+}
+
+const IDLE = new State({
+    enter: () => {
+        time = DEFAULT_TIME;
+        update_text(time);
+    },
+    button_label: 'Start',
+});
+const RUN = new State({
+    enter: () => start(time),
+    exit: () => clearInterval(interval_id),
+    button_label: 'Pause'
+});
+const PAUSED = new State({
+    text_class: 'pause',
+    button_label: 'Continue'
+});
+const ALARM = new State({
+    enter: () => {
+        time = 0;
+        // TODO start playing sound
+    },
+    exit: () => {
+        // TODO stop playing sound
+    },
+    text_class: 'alarm',
+    button_label: 'Snooze'
+});
+const COOLDOWN = new State({
+    enter: () => {
+        ctrl_button.disabled = true;
+        start(COOLDOWN_TIME);
+    },
+    exit: () => {
+        clearInterval(interval_id);
+        ctrl_button.disabled = false;
+    },
+    button_label: 'Come back soon!'
+});
+
+let state;
+
+function switch_state(new_state) {
+    if (state) state.exit();
+    new_state.enter();
+    state = new_state;
+}
+
+function start(t) {
+    time = t;
+    t0 = new Date();
+    interval_id = setInterval(tick, 100);
+}
+
+function tick() {
+    let t1 = new Date();
+    let dt = t1 - t0;
+    time -= dt;
+
+    if (time <= 0) {
+        switch_state(state == COOLDOWN ? IDLE : ALARM);
+    }
+
+    update_text(time);
+    t0 = t1;
+}
+
+function ctrl() {
+    let new_state;
+    switch (state) {
+        case IDLE: new_state = RUN; break;
+        case RUN: new_state = PAUSED; break;
+        case PAUSED: new_state = RUN; break;
+        case ALARM: new_state = COOLDOWN; break;
+    }
+    switch_state(new_state);
+}
+
 function init() {
+    timer_element = document.getElementById('timer');
+    ctrl_button = document.getElementById('ctrl');
+
+    ctrl_button.addEventListener('click', () => ctrl());
+    window.addEventListener('keydown', (e) => {
+        if (e.key !== ' ') return;
+        if (e.repeat) return;
+        ctrl();
+    });
+
+    switch_state(IDLE);
 }
